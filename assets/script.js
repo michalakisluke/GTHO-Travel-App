1cd5eaf675896959aa2c39c0e5856c322d704864
Sarah Dickinson <sarahadickinson@gmail.com>
2021-09-21 10:58:01 -0400

    add search behavior to hide container/unhide spinner

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,19 @@ apiKey = "12524a4796d1cd5b9b5d525171960baf";
 let airportArrayClear = [];
 
 // Search button click
-$(".uk-button-secondary").on("click", function (event){
+$(".uk-button-secondary").on("click", function(event) {
     event.preventDefault();
     zip = $("#user-zip").val().trim();
     if (zip === '') {
         console.log("You woulda thought");
         return;
-    }
-    else {
+    } else {
         console.log(zip);
         localTempApiFetch();
+
+        // show spinner & hide user input upon click
+        $("#spinner").removeAttr("hidden");
+        $("#location").attr("hidden", true);
     }
 });
 
@@ -26,32 +29,34 @@ function checkNull() {
 //Fetch temp data for zip codes
 function localTempApiFetch() {
     // Write a fetch request to the Giphy API
-    fetch("https://api.openweathermap.org/data/2.5/weather?zip="+zip+"&units=imperial&appid="+apiKey)
-    .then(function(response){
-        return response.json();
-    }).then(function(response) {
-        var localTemp = response.main.temp;
-        var localLat = response.coord.lat;
-        var localLong = response.coord.lon;
-        console.log("The local temp is " + localTemp + " degrees Farenheit");
-        console.log("The local latitude is " + localLat);
-        console.log("The local longitude is " + localLong);
-        fetch("https://aviation-reference-data.p.rapidapi.com/airports/search?lat="+ localLat +"&lon="+ localLong +"&radius=50", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "aviation-reference-data.p.rapidapi.com",
-                "x-rapidapi-key": "862a716dc7msh647274362d7a08cp12fe37jsn54b5d3acd3b7"
-            }
-        })
-        .then(function(response){
+    fetch("https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&units=imperial&appid=" + apiKey)
+        .then(function(response) {
             return response.json();
-        }).then(function(response){
-            airportArray = response;
-            let airportArrayClear = airportArray.filter(function(e){
-                return e.icaoCode != null;
-            });
-            console.log(airportArrayClear);
+        }).then(function(response) {
+            var localTemp = response.main.temp;
+            var localLat = response.coord.lat;
+            var localLong = response.coord.lon;
+            console.log("The local temp is " + localTemp + " degrees Farenheit");
+            console.log("The local latitude is " + localLat);
+            console.log("The local longitude is " + localLong);
+            fetch("https://aviation-reference-data.p.rapidapi.com/airports/search?lat=" + localLat + "&lon=" + localLong + "&radius=50", {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-host": "aviation-reference-data.p.rapidapi.com",
+                        "x-rapidapi-key": "862a716dc7msh647274362d7a08cp12fe37jsn54b5d3acd3b7"
+                    }
+                })
+                .then(function(response) {
+                    return response.json();
+                }).then(function(response) {
+                    airportArray = response;
+                    let airportArrayClear = airportArray.filter(function(e) {
+                        return e.icaoCode != null;
+                    });
+                    console.log(airportArrayClear);
 
+                });
         });
-    });
 }
+
+// function to pop results
