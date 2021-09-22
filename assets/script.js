19e3eb56f1c58479f02f8bbdf9097ef42996d6f5
Luke Michalakis <michalakisluke@gmail.com>
2021-09-22 13:08:29 -0400

    <Build airport temp sorting function>

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 apiKey = "12524a4796d1cd5b9b5d525171960baf";
 let airportArrayClear = [];
+let tempCheckArray = [];
 
 // Search button click
 $(".uk-button-secondary").on("click", function(event) {
@@ -18,14 +19,6 @@ $(".uk-button-secondary").on("click", function(event) {
     }
 });
 
-function checkNull() {
-    for (i = 0; i > airportArray; i++) {
-        if (airportArray[i] == null) {
-
-        }
-    }
-}
-
 //Fetch temp data for zip codes
 function localTempApiFetch() {
     // Write a fetch request to the Giphy API
@@ -39,7 +32,8 @@ function localTempApiFetch() {
             console.log("The local temp is " + localTemp + " degrees Farenheit");
             console.log("The local latitude is " + localLat);
             console.log("The local longitude is " + localLong);
-            fetch("https://aviation-reference-data.p.rapidapi.com/airports/search?lat=" + localLat + "&lon=" + localLong + "&radius=250", {
+            // Find airports within a given radius, pull lat and lon coordinates
+            fetch("https://aviation-reference-data.p.rapidapi.com/airports/search?lat=" + localLat + "&lon=" + localLong + "&radius=500", {
                     "method": "GET",
                     "headers": {
                         "x-rapidapi-host": "aviation-reference-data.p.rapidapi.com",
@@ -53,7 +47,26 @@ function localTempApiFetch() {
                     let airportArrayClear = airportArray.filter(function(e) {
                         return e.icaoCode != null;
                     });
-                    console.log(airportArrayClear);
+                    let airportArrayCheckLat = airportArrayClear.filter(function(lat){
+                        return lat.latitude < localLat;
+                    });
+                    // Find temp at each airport
+                    for (j = 0; j < airportArrayCheckLat.length; j++) {
+                        var tempLatCheck = airportArrayCheckLat[j].latitude;
+                        var tempLonCheck = airportArrayCheckLat[j].longitude;
+                        fetch("https://api.openweathermap.org/data/2.5/weather?lat="+ tempLatCheck + "&lon=" + tempLonCheck + "&units=imperial&appid=" + apiKey)
+                        .then(function(response){
+                            return response.json();
+                        }).then(function(response){
+                            var airTemp = response.main.temp;
+                            var latAtAirport = response.coord.lat;
+                            var lonAtAirport = response.coord.lon;
+                            var nestedArrayElem = {"latitude": latAtAirport, "longitude": lonAtAirport, "temp": airTemp};
+                            //console.log(nestedArrayElem);
+                            tempCheckArray.push(nestedArrayElem);
+                        });
+                    };
+                    console.log(tempCheckArray);
                 });
         });
 }
